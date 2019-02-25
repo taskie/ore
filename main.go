4aed86e7a8e6c4e7cfc08caeb3722119037955bb
taskie <t@skie.jp>
2019-02-26 02:23:25 +0900

    use cobra & viper

--- main.go
+++ main.go
@@ -1,11 +1,20 @@
 package main
 
 import (
-	"fmt"
-	"sort"
-	"strings"
+	"os"
+	"path/filepath"
 
-	"github.com/taskie/cliplus"
+	log "github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/taskie/csvt/cli/csvt"
+	"github.com/taskie/fwv/cli/fwv"
+	"github.com/taskie/gfp/cli/gfp"
+	"github.com/taskie/gtp/cli/gtp"
+	"github.com/taskie/jc/cli/jc"
+	"github.com/taskie/osplus"
+	"github.com/taskie/pity/cli/pity"
+	"github.com/taskie/reinc/cli/reinc"
+	"github.com/taskie/rltee/cli/rltee"
 )
 
 var (
@@ -13,34 +22,96 @@ var (
 	revision string
 )
 
-func main() {
-	cmd := cliplus.NewBusyCmd("ore", cliplus.NewMapMainResolver(getCommands()))
-	cmd.Main()
+const (
+	CommandName = "ore"
+)
+
+var Command = &cobra.Command{
+	Use: CommandName,
 }
 
-func list() {
-	names := make([]string, 0, 0)
-	for name := range getCommands() {
-		if !strings.HasPrefix(name, "-") {
-			names = append(names, name)
+func init() {
+	Command.AddCommand(jc.Command)
+	Command.AddCommand(gtp.Command)
+	Command.AddCommand(csvt.Command)
+	Command.AddCommand(fwv.Command)
+	Command.AddCommand(reinc.Command)
+	Command.AddCommand(pity.Command)
+	Command.AddCommand(rltee.Command)
+	Command.AddCommand(gfp.Command)
+
+	cmdNames := make([]string, 0)
+	for _, cmd := range Command.Commands() {
+		cmdNames = append(cmdNames, cmd.Name())
+	}
+
+	Command.AddCommand(&cobra.Command{
+		Use: "link",
+		Run: func(cmd *cobra.Command, args []string) {
+			gopath, err := osplus.GetGoPath()
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, cmdName := range cmdNames {
+				dst := filepath.Join(gopath, "bin", cmdName)
+				abs, err := filepath.Abs(os.Args[0])
+				if err != nil {
+					log.Warn(err)
+					continue
+				}
+				err = os.Symlink(abs, dst)
+				if err != nil {
+					log.Warn(err)
+				}
+			}
+		},
+	})
+	Command.AddCommand(&cobra.Command{
+		Use: "unlink",
+		Run: func(cmd *cobra.Command, args []string) {
+			gopath, err := osplus.GetGoPath()
+			if err != nil {
+				log.Fatal(err)
+			}
+			for _, cmdName := range cmdNames {
+				dst := filepath.Join(gopath, "bin", cmdName)
+				err = os.Remove(dst)
+				if err != nil {
+					log.Warn(err)
+				}
+			}
+		},
+	})
+	Command.AddCommand(&cobra.Command{
+		Use: "bash",
+		Run: func(cmd *cobra.Command, args []string) {
+			Command.GenBashCompletion(os.Stdout)
+		},
+	})
+	Command.AddCommand(&cobra.Command{
+		Use: "zsh",
+		Run: func(cmd *cobra.Command, args []string) {
+			Command.GenZshCompletion(os.Stdout)
+		},
+	})
+}
+
+func main() {
+	base := filepath.Base(os.Args[0])
+	if base == CommandName {
+		err := Command.Execute()
+		if err != nil {
+			log.Fatal(err)
+		}
+	} else {
+		cmd, ss, err := Command.Find(append([]string{base}, os.Args[1:]...))
+		if err != nil {
+			log.Fatal(err)
+		}
+		os.Args = append([]string{CommandName, base}, ss...)
+		err = cmd.Execute()
+		if err != nil {
+			log.Fatal(err)
 		}
 	}
-	sort.Strings(names)
-	for _, name := range names {
-		fmt.Println(name)
-	}
-}
-
-func showVersion() {
-	fmt.Println(version)
-}
-
-const usage = `ore - my own toolchain
-usage: ore COMMAND ARGS...
-       ore list
-detail: https://github.com/taskie/ore
-`
-
-func help() {
-	fmt.Print(usage)
 }
